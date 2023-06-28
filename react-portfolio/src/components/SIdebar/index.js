019d62ffd1049ca118b82f52524cc1fe2fb201c6
givingbackShishir <shishir@givingback.ai>
2023-06-28 09:34:21 +0545

    test

--- react-portfolio/src/components/SIdebar/index.js
+++ react-portfolio/src/components/SIdebar/index.js
@@ -1,15 +1,16 @@
-import './index.scss';
-import { Link } from 'react-router-dom';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+import './index.scss'
+import { Link } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img sec={LogoS} alt='logo'/>
-            <img sec={LogoSubtitle} alt='slobodan'/>
-        </Link>
-    </div>
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img sec={LogoS} alt="logo" />
+      <img sec={LogoSubtitle} alt="slobodan" />
+      <>Test</>
+    </Link>
+  </div>
 )
 
-export default Sidebar;
+export default Sidebar
